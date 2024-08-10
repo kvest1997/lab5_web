55e1c8a8539a459f4e072cfafbe41a3df231891a
Kirill <kvest1997@gmail.com>
2024-08-10 13:30:01 +0700

    u5

--- flaskapp/client.py
+++ flaskapp/client.py
@@ -1,7 +1,5 @@
 import requests
-r = requests.get('http://localhost:5000/')
+
+r = requests.get('http://127.0.0.1:5000/')
 print(r.status_code)
 print(r.text)
-r = requests.get('http://localhost:5000/data_to')
-print(r.status_code)
-print(r.text) 
--- flaskapp/st.sh
+++ flaskapp/st.sh
@@ -1,8 +1,11 @@
+#!/bin/bash
+
 gunicorn --bind 127.0.0.1:5000 wsgi:app & APP_PID=$!
 sleep 5
-echo start client
+echo "Starting client test"
 python3 client.py
 sleep 5
 echo $APP_PID
 kill -TERM $APP_PID
-exit 0 
+echo "Gunicorn process killed"
+exit 0
